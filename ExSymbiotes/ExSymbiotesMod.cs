6ca033f22dc09aae619734c2ea41233036109c53
NicolasIannello <nicolas.iannello99@gmail.com>
2026-08-17 14:51:20 -0300

    initial

---
+++ ExSymbiotes/ExSymbiotesMod.cs
@@ -0,0 +1,15 @@
+﻿using Verse;
+
+namespace ExSymbiotes
+{
+    public class ExSymbiotesMod : Mod
+    {
+        public static ExSymbiotesMod Instance;
+
+        public ExSymbiotesMod(ModContentPack content) : base(content)
+        {
+            Instance = this;
+        }
+
+    }
+}
