0173d67913e734693e119233f5fc209615f7f574
NicolasIannello <nicolas.iannello99@gmail.com>
2026-08-18 16:21:23 -0300

    CompSymbioteMass_Black

---
+++ ExSymbiotes/CompSymbioteMass_Black.cs
@@ -0,0 +1,43 @@
+﻿using Verse;
+using RimWorld;
+
+namespace ExSymbiotes
+{
+  public class CompSymbioteMass_Black : CompObelisk_ExplodingSpawner
+  {
+
+    public override void TriggerInteractionEffect(Pawn interactor, bool triggeredByPlayer = false)
+    {
+      Log.Message(interactor.NameFullColored+" "+triggeredByPlayer);
+      if (triggeredByPlayer)
+      {
+        ThingDef serumDef = ThingDef.Named("VoidsightSerum");
+        Thing serum = ThingMaker.MakeThing(serumDef);
+        serum.stackCount = 1;
+
+        GenPlace.TryPlaceThing(serum, parent.Position, parent.Map, ThingPlaceMode.Direct);
+      }
+      else
+      {
+        
+      }
+    }
+
+    public override void OnActivityActivated()
+    {
+      base.OnActivityActivated();
+      Log.Message("OnActivityActivated");
+    }
+
+    public override void CompTick()
+    {
+      base.CompTick();
+      Log.Message("CompTick");
+    }
+    
+    // public override void PostExposeData()
+    // {
+    //   base.PostExposeData();
+    // }
+  }
+}
