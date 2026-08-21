f2cde2d0636948204916bbd97b14af79a8bd591e
NicolasIannello <nicolas.iannello99@gmail.com>
2026-08-21 15:45:08 -0300

    psychic ritual

---
+++ ExSymbiotes/Ritual/PsychicRitualDef_SummonSymbioticMass.cs
@@ -0,0 +1,29 @@
+﻿using RimWorld;
+using Verse;
+using Verse.AI.Group;
+using System.Collections.Generic;
+
+namespace ExSymbiotes
+{
+    public class PsychicRitualDef_SummonSymbioticMass : PsychicRitualDef_InvocationCircle
+    {
+
+        public override List<PsychicRitualToil> CreateToils(
+            PsychicRitual psychicRitual, PsychicRitualGraph graph)
+        {
+            List<PsychicRitualToil> toils = base.CreateToils(psychicRitual, graph);
+            toils.Add((PsychicRitualToil) new PsychicRitualToil_SummonSymbioticMass(this.InvokerRole));
+            return toils;
+        }
+
+        public override IEnumerable<string> BlockingIssues(
+            PsychicRitualRoleAssignments assignments,
+            Map map)
+        {
+            foreach (string blockingIssue in base.BlockingIssues(assignments, map))
+                yield return blockingIssue;
+            if (map.listerThings.ThingsOfDef(ExSymbiotesDefOf.ExSymbiotes_SymbioteMass_Black).Count > 0 || map.listerThings.ThingsOfDef(ExSymbiotesDefOf.ExSymbiotes_SymbioteMassIncoming).Count > 0)
+                yield return (string) "PitGateAlreadyExists".Translate();
+        }
+    }
+}
---
+++ ExSymbiotes/Ritual/PsychicRitualToil_SummonSymbioticMass.cs
@@ -0,0 +1,45 @@
+﻿using RimWorld;
+using Verse;
+using Verse.AI.Group;
+
+namespace ExSymbiotes
+{
+    public class PsychicRitualToil_SummonSymbioticMass : PsychicRitualToil
+    {
+        private PsychicRitualRoleDef invokerRole;
+
+        protected PsychicRitualToil_SummonSymbioticMass() { }
+
+        public PsychicRitualToil_SummonSymbioticMass(PsychicRitualRoleDef invokerRole)
+        {
+            this.invokerRole = invokerRole;
+        }
+
+        public override void Start(PsychicRitual psychicRitual, PsychicRitualGraph parent)
+        {
+            base.Start(psychicRitual, parent);
+            Pawn invoker = psychicRitual.assignments.FirstAssignedPawn(this.invokerRole);
+            psychicRitual.ReleaseAllPawnsAndBuildings();
+            if (invoker == null)
+                return;
+            
+            this.ApplyOutcome(psychicRitual, invoker);
+        }
+
+        private void ApplyOutcome(PsychicRitual psychicRitual, Pawn invoker)
+        {
+            Find.Storyteller.incidentQueue.Add(ExSymbiotesDefOf.ExSymbiotes_SymbioteMass_Incident, Find.TickManager.TicksGame, new IncidentParms()
+            {
+                target = (IIncidentTarget) invoker.Map,
+                pointMultiplier = StorytellerUtility.DefaultThreatPointsNow((IIncidentTarget) invoker.Map),
+                forced = true
+            });
+        }
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Defs.Look<PsychicRitualRoleDef>(ref this.invokerRole, "invokerRole");
+        }
+    }
+}
