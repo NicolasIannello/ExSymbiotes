b63541f23ff8600f42d4b90e8d45274e1ef4b836
NicolasIannello <nicolas.iannello99@gmail.com>
2026-08-26 13:23:23 -0300

    CutoutSkin blue tint

--- ExSymbiotes/Patches/Symbiosis.cs
+++ ExSymbiotes/Patches/Symbiosis.cs
@@ -11,26 +11,31 @@ namespace ExSymbiotes
     {
         private static readonly Dictionary<Material, Material> materials = new Dictionary<Material, Material>();
 
-        public static void Postfix(PawnRenderNode node, PawnDrawParms parms, ref Material __result)
+        public static void Postfix(PawnRenderNodeWorker __instance, PawnRenderNode node, PawnDrawParms parms, ref Material __result)
         {
             if (__result == null) return;
             Pawn pawn = parms.pawn;
             if (pawn == null) return;
             if (!HediffComp_SymbioticArmor.SymbioticArmorWeakTable.TryGetValue(pawn, out HediffComp_SymbioticArmor _)) return;
-
+            if (__instance is PawnRenderNodeWorker_Eye) return;
+            
             GraphicStateDef state;
             GraphicStateDef graphicState = !parms.flags.FlagSet(PawnRenderFlags.Portrait) && node.TryGetAnimationGraphicState(parms, out state) ? state : (GraphicStateDef) null;
             Graphic graphic = graphicState == null ? node.PrimaryGraphic : node.GraphicForState(graphicState);
-            
-            // if (graphic == null) return;
-            // if (node.Props.flipGraphic && parms.facing.IsHorizontal) parms.facing = parms.facing.Opposite;
             Material baseMat = graphic.NodeGetMat(parms);
             Material myMat;
+            
             if (!materials.TryGetValue(baseMat, out myMat))
             {
-                myMat = InvisibilityMatPool.GetInvisibleMat(baseMat);
-                myMat.shader = ShaderDatabase.Cutout;// Cutout CutoutSkin Metalblood;
+                if (!UnityData.IsInMainThread)
+                {
+                    Log.Error("Anomaly Symbiotes: Attempted to create duplicate material off main thread, do this in ensure materials initialized.");
+                    return;
+                }
+                myMat = new Material(InvisibilityMatPool.GetInvisibleMat(baseMat));
+                myMat.shader = ShaderDatabase.CutoutSkin; // Cutout CutoutSkin Metalblood;
                 myMat.color = pawn.story.SkinColor;
+                myMat.SetColor("_ShadowColor", new Color(0.118f, 0, 0.812f));
                 materials.Add(baseMat, myMat);
             }
             
