b2bbd48f5ae4e527f4907133377a62b142c81d2a
NicolasIannello <nicolas.iannello99@gmail.com>
2026-08-27 15:55:16 -0300

    symbiosis bar

--- ExSymbiotes/Hediff/Symbiosis/HediffCompProperties_Symbiosis.cs
+++ ExSymbiotes/Hediff/Symbiosis/HediffCompProperties_Symbiosis.cs
@@ -6,8 +6,8 @@ namespace ExSymbiotes
     public class HediffCompProperties_Symbiosis : HediffCompProperties
     {
         public Color color;
-        // public float minDrawSize = 1.2f;
-        // public float maxDrawSize = 1.55f;
+        public float minDrawSize = 1.2f;
+        public float maxDrawSize = 1.55f;
         
         public HediffCompProperties_Symbiosis() => this.compClass = typeof (HediffComp_Symbiosis);
     }
