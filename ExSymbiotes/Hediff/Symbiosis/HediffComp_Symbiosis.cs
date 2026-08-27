e29d7c5d9c70bbffbd41b40114eb5ef0a6da30de
NicolasIannello <nicolas.iannello99@gmail.com>
2026-08-27 14:53:13 -0300

    symbiosis bar

--- ExSymbiotes/Hediff/Symbiosis/HediffComp_Symbiosis.cs
+++ ExSymbiotes/Hediff/Symbiosis/HediffComp_Symbiosis.cs
@@ -1,11 +1,17 @@
-﻿using Verse;
+﻿using System;
+using System.Collections.Generic;
+using RimWorld;
+using Verse;
 
 namespace ExSymbiotes
 {
     public class HediffComp_Symbiosis: HediffComp
     {
-        private HediffCompProperties_Symbiosis Props => (HediffCompProperties_Symbiosis) this.props;
-
+        public HediffCompProperties_Symbiosis Props => (HediffCompProperties_Symbiosis) this.props;
+        protected int energy;
+        public int Energy => this.energy;
+        public int EnergyMax = 100;
+        
         public override void CompPostPostAdd(DamageInfo? dinfo)
         {
             Pawn.story.skinColorOverride = this.Props.color;
@@ -14,6 +20,43 @@ namespace ExSymbiotes
         public override void CompPostPostRemoved()
         {
             Pawn.story.skinColorOverride = null;
+        }
+        
+        public override IEnumerable<Gizmo> CompGetGizmos()
+        {
+            IEnumerable<Gizmo> compGetGizmos = base.CompGetGizmos();
+            if (compGetGizmos != null) foreach (Gizmo gizmo in compGetGizmos) yield return gizmo;
+            
+            IEnumerable<Gizmo> getGizmos = this.GetGizmos();
+            if (getGizmos != null) foreach (Gizmo gizmo in getGizmos) yield return gizmo;
+
+            if (DebugSettings.ShowDevGizmos)
+            {
+                Command_Action commandAction1 = new Command_Action();
+                commandAction1.defaultLabel = "DEV: Fill";
+                commandAction1.action = new Action(this.Fill);
+                yield return (Gizmo) commandAction1;
+            }
+        }
+        
+        private IEnumerable<Gizmo> GetGizmos()
+        {
+            if ((this.Pawn.Faction == Faction.OfPlayer || this.Pawn is Pawn pawn && pawn.RaceProps.IsMechanoid) && Find.Selector.SingleSelectedThing == this.Pawn)
+                yield return (Gizmo) new Gizmo_Symbiosis(Props.color)
+                {
+                    shield = this
+                };
+        }
+        
+        private void Fill()
+        {
+            this.energy = 100;
+        }
+        
+        public override void CompExposeData()
+        {
+            base.CompExposeData();
+            Scribe_Values.Look<int>(ref this.energy, "energy");
         }
     }
 }
---
+++ ExSymbiotes/UI/Gizmo_Symbiosis.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using Verse;
+
+namespace ExSymbiotes
+{
+    [StaticConstructorOnStartup]
+    public class Gizmo_Symbiosis : Gizmo
+    {
+        public HediffComp_Symbiosis shield;
+        private readonly Texture2D FullShieldBarTex;
+        static readonly Texture2D EmptyShieldBarTex = SolidColorMaterials.NewSolidColorTexture(Color.clear);
+
+        public Gizmo_Symbiosis(Color color) {
+            this.Order = -100f;
+            FullShieldBarTex = SolidColorMaterials.NewSolidColorTexture(color);
+        }
+
+        public override float GetWidth(float maxWidth) => 140f;
+
+        public override GizmoResult GizmoOnGUI(Vector2 topLeft, float maxWidth, GizmoRenderParms parms)
+        {
+            Rect rect1 = new Rect(topLeft.x, topLeft.y, this.GetWidth(maxWidth), 75f);
+            Rect rect2 = rect1.ContractedBy(6f);
+            Widgets.DrawWindowBackground(rect1);
+            Rect rect3 = new Rect(rect2.x, rect2.y, rect2.width, rect1.height / 2f);
+            Text.Font = GameFont.Tiny;
+            Widgets.Label(rect3, "ExSymbiotes.Symbiosis".Translate());
+            Rect rect4 = new Rect(rect2.x, rect2.y + (rect2.height / 2f), rect2.width, rect2.height / 2f);
+            float fillPercent = this.shield.Energy / Mathf.Max(1f, this.shield.EnergyMax);
+            Widgets.FillableBar(rect4, fillPercent, FullShieldBarTex, Gizmo_Symbiosis.EmptyShieldBarTex, false);
+            Text.Font = GameFont.Small;
+            Text.Anchor = TextAnchor.MiddleCenter;
+            Widgets.Label(rect4, $"{(this.shield.Energy).ToString("F0")} / {(this.shield.EnergyMax).ToString("F0")}");
+            Text.Anchor = TextAnchor.UpperLeft;
+            TooltipHandler.TipRegion(rect2, (TipSignal) "ExSymbiotes.SymbiosisTip".Translate());
+            return new GizmoResult(GizmoState.Clear);
+        }
+    } 
+}
