7229b12df923fa09b0a54357b66e6a1d1e00bb07
NicolasIannello <nicolas.iannello99@gmail.com>
2026-09-10 15:05:36 -0300

    render node eye

---
+++ ExSymbiotes/Render/PawnRenderNodeProperties_EyeSymbiote.cs
@@ -0,0 +1,10 @@
+﻿using RimWorld;
+using Verse;
+
+namespace ExSymbiotes
+{
+    public class PawnRenderNodeProperties_EyeSymbiote : PawnRenderNodeProperties_Eye
+    {
+        public override void ResolveReferences() => this.skipFlag = RenderSkipFlagDefOf.None;
+    }
+}
