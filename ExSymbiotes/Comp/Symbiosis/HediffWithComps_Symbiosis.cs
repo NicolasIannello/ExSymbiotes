7d394313011ad12e693345b4385fc9ac197592d4
NicolasIannello <nicolas.iannello99@gmail.com>
2026-09-13 19:13:49 -0300

    chocolate

---
+++ ExSymbiotes/Comp/Symbiosis/HediffWithComps_Symbiosis.cs
@@ -0,0 +1,14 @@
+﻿using RimWorld;
+using Verse;
+
+namespace ExSymbiotes
+{
+    public class HediffWithComps_Symbiosis : HediffWithComps
+    {
+        public override void Notify_IngestedThing(Thing thing, int amount)
+        {
+            base.Notify_IngestedThing(thing, amount);
+            if(thing.def == ThingDefOf.Chocolate) this.TryGetComp<HediffComp_Symbiosis>().AddSymbiosis(4);
+        }
+    }
+}
