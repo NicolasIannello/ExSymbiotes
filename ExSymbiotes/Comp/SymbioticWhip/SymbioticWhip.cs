be734c6a58536851577a738a024188869c5b2dcd
NicolasIannello <nicolas.iannello99@gmail.com>
2026-09-20 16:05:42 -0300

    miss checks

---
+++ ExSymbiotes/Comp/SymbioticWhip/SymbioticWhip.cs
@@ -0,0 +1,17 @@
+﻿using RimWorld;
+using Verse;
+
+namespace ExSymbiotes
+{
+    public class SymbioticWhip : Bullet
+    {
+
+        protected override void Impact(Thing hitThing, bool blockedByShield = false)
+        {
+            Pawn pawn = intendedTarget.Pawn;
+            if (hitThing == null && (pawn.Downed || pawn.Faction == Faction.OfPlayer)) hitThing = pawn;
+            base.Impact(hitThing, blockedByShield);
+        }
+        
+    }
+}
