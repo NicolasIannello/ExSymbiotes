e84bb3f662451d0f35e904091b51e2522d2c6a20
NicolasIannello <nicolas.iannello99@gmail.com>
2026-09-22 16:59:43 -0300

    filth

--- ExSymbiotes/Comp/VenomLeap/CompAbilityEffect_VenomLeap.cs
+++ ExSymbiotes/Comp/VenomLeap/CompAbilityEffect_VenomLeap.cs
@@ -11,8 +11,11 @@ namespace ExSymbiotes
         public override void Apply(LocalTargetInfo target, LocalTargetInfo dest)
         {
             base.Apply(target, dest);
-            if(this.parent.pawn.health.hediffSet.HasHediff(ExSymbiotesDefOf.ExSymbiotes_Symbiosis))
+            if (this.parent.pawn.health.hediffSet.HasHediff(ExSymbiotesDefOf.ExSymbiotes_Symbiosis))
+            {
                 this.parent.pawn.health.GetOrAddHediff(ExSymbiotesDefOf.ExSymbiotes_Symbiosis).TryGetComp<HediffComp_VenomLeap>().SetStage(true);
+                SymbioteUtility.MeatSplatter(0, this.parent.pawn.Position, this.parent.pawn.Map, FleshbeastUtility.MeatExplosionSize.Small);
+            }
         }
         
         public void OnJumpCompleted(IntVec3 origin, LocalTargetInfo target)
@@ -21,6 +24,7 @@ namespace ExSymbiotes
                 this.parent.pawn.health.GetOrAddHediff(ExSymbiotesDefOf.ExSymbiotes_Symbiosis).TryGetComp<HediffComp_VenomLeap>().SetStage(false);
             GenExplosion.DoExplosion(this.parent.pawn.Position, this.parent.pawn.Map, this.Props2.radius, ExSymbiotesDefOf.ExSymbiotes_VenomLeap, this.parent.pawn, doVisualEffects:false, doSoundEffects:false, screenShakeFactor:0f, excludeRadius:1f);
             SymbioteUtility.MeatSplatter(0, this.parent.pawn.Position, this.parent.pawn.Map, FleshbeastUtility.MeatExplosionSize.Small);
+            FilthMaker.TryMakeFilth(this.parent.pawn.Position, this.parent.pawn.Map, this.Props2.filth);
         }
     }
 }
--- ExSymbiotes/Comp/VenomLeap/CompProperties_VenomLeap.cs
+++ ExSymbiotes/Comp/VenomLeap/CompProperties_VenomLeap.cs
@@ -1,10 +1,12 @@
 ﻿using RimWorld;
+using Verse;
 
 namespace ExSymbiotes
 {
     public class CompProperties_VenomLeap : CompProperties_AbilityEffect
     {
         public float radius;
+        public ThingDef filth;
         
         public CompProperties_VenomLeap() => this.compClass = typeof (CompAbilityEffect_VenomLeap);
     }
