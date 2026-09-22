54c3a8bdd90895cb62ed54ad8661cc317dd237cc
NicolasIannello <nicolas.iannello99@gmail.com>
2026-09-22 17:07:51 -0300

    custom target verb

---
+++ ExSymbiotes/Comp/VenomLeap/Verb_CastAbilityVenomLeap.cs
@@ -0,0 +1,21 @@
+﻿using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace ExSymbiotes
+{
+    public class Verb_CastAbilityVenomLeap : Verb_CastAbilityJump
+    {
+        public override ThingDef JumpFlyerDef => ExSymbiotesDefOf.ExSymbiotes_PawnFlyer_VenomLeap;
+        private float radius = 2.5f;
+        
+        public override void DrawHighlight(LocalTargetInfo target)
+        {
+            base.DrawHighlight(target);
+            if (target.IsValid && JumpUtility.ValidJumpTarget((Thing)this.CasterPawn, this.caster.Map, target.Cell))
+            {
+                GenDraw.DrawRadiusRing(target.Cell, radius, Color.white);
+            }
+        }
+    }
+}
