1382f215723c5bf3fdd3f8ef4e4eacb1e6d54e05
NicolasIannello <nicolas.iannello99@gmail.com>
2026-09-22 15:21:03 -0300

    dead check

--- ExSymbiotes/Comp/SymbioticStun/DamageWorker_SymbioticStun.cs
+++ ExSymbiotes/Comp/SymbioticStun/DamageWorker_SymbioticStun.cs
@@ -9,7 +9,7 @@ namespace ExSymbiotes
         public override DamageWorker.DamageResult Apply(DamageInfo dinfo, Thing victim)
         {
             DamageWorker.DamageResult damageResult = base.Apply(dinfo, victim);
-            if (victim is Pawn pawn)
+            if (victim is Pawn pawn && !pawn.Dead)
             {
                 pawn.stances.stunner.StunFor((int)dinfo.Def.constantStunDurationTicks, dinfo.Instigator, showMote:false);
                 MoteMaker.MakeAttachedOverlay(pawn, ExSymbiotesDefOf.ExSymbiotes_Mote_HarbingerTreeRoots, new Vector3(), pawn.BodySize, (float)dinfo.Def.constantStunDurationTicks/60);
